d20697ecc23200b5b429c89256383dde35bab3a2
theomorphic <FelixMK@yandex.ru>
2024-03-24 19:01:30 +0500

    fix: delete the animation

--- js/script.js
+++ js/script.js
@@ -4,9 +4,9 @@ const mainFace = document.getElementById("mainFace");
 const body = document.getElementById("body");
 
 (function(){
-	setTimeout(() => {
-		mainFace.style.backgroundImage = "url(../img/emotions/wondering.png)";
-	}, 2500);
+	// setTimeout(() => {
+	// 	mainFace.style.backgroundImage = "url(../img/emotions/wondering.png)";
+	// }, 2500);
 	setTimeout(() => {
 		body.classList.remove("start");
 		mainFace.classList.remove("start");
@@ -15,7 +15,7 @@ const body = document.getElementById("body");
 		console.log("%cHe's laid-back and ready to talk",
 		"color: #1e7062; font-size: 14px")
 	}, 4000);
-	setTimeout(() => {
-		mainFace.style.backgroundImage = "";
-	}, 5000);
+	// setTimeout(() => {
+	// 	mainFace.style.backgroundImage = "";
+	// }, 5000);
 })();
