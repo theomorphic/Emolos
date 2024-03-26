c52e10b04501b7ec379788a98fdcf77584473030
theomorphic <FelixMK@yandex.ru>
2024-03-26 13:44:25 +0500

    test: put inline script

--- js/script.js
+++ js/script.js
@@ -1,35 +1,35 @@
 
-const mainFace = document.getElementById("mainFace");
+// const mainFace = document.getElementById("mainFace");
 
-const body = document.getElementById("body");
+// const body = document.getElementById("body");
 
 
-(function(){
-	setTimeout(() => {
-		mainFace.classList.add("wondering");
-	}, 2500);
-	setTimeout(() => {
-		body.classList.remove("start");
-		mainFace.classList.remove("start");
+// (function(){
+// 	setTimeout(() => {
+// 		mainFace.classList.add("wondering");
+// 	}, 2500);
+// 	setTimeout(() => {
+// 		body.classList.remove("start");
+// 		mainFace.classList.remove("start");
 
-		console.log("%cHe's laid-back and ready to talk",
-		"color: #1e7062; font-size: 14px")
-	}, 4000);
-	setTimeout(() => {
-		mainFace.style.backgroundImage = "";
-		mainFace.classList.remove("wondering");
+// 		console.log("%cHe's laid-back and ready to talk",
+// 		"color: #1e7062; font-size: 14px")
+// 	}, 4000);
+// 	setTimeout(() => {
+// 		mainFace.style.backgroundImage = "";
+// 		mainFace.classList.remove("wondering");
 
-	}, 5000);
-})();
+// 	}, 5000);
+// })();
 
-//ускорение hover
+// //ускорение hover
 
-mainFace.onmouseover = hover;
-mainFace.onmouseout = noHover;
-function hover(){
-	mainFace.classList.add("looking");
-};
-function noHover(){
-	mainFace.classList.remove("looking");
-};
+// mainFace.onmouseover = hover;
+// mainFace.onmouseout = noHover;
+// function hover(){
+// 	mainFace.classList.add("looking");
+// };
+// function noHover(){
+// 	mainFace.classList.remove("looking");
+// };
 
