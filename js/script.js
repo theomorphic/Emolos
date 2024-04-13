eec73da28e09e28f5847bd9a6975cc96a86a1fb3
theomorphic <FelixMK@yandex.ru>
2024-04-14 01:05:37 +0500

    feat: add blinking

--- js/script.js
+++ js/script.js
@@ -4,23 +4,23 @@ const mainFace = document.getElementById("mainFace");
 const body = document.getElementById("body");
 
 
-(function(){
-	setTimeout(() => {
-		mainFace.classList.add("wondering");
-	}, 2500);
-	setTimeout(() => {
-		body.classList.remove("start");
-		mainFace.classList.remove("start");
+// (function(){
+// 	setTimeout(() => {
+// 		mainFace.classList.add("wondering");
+// 	}, 2500);
+// 	setTimeout(() => {
+// 		body.classList.remove("start");
+// 		mainFace.classList.remove("start");
 
-		console.log("%cHe's laid-back and ready to talk",
-		"color: #1e7062; font-size: 14px")
-	}, 4000);
-	setTimeout(() => {
-		mainFace.style.backgroundImage = "";
-		mainFace.classList.remove("wondering");
+// 		console.log("%cHe's laid-back and ready to talk",
+// 		"color: #1e7062; font-size: 14px")
+// 	}, 4000);
+// 	setTimeout(() => {
+// 		mainFace.style.backgroundImage = "";
+// 		mainFace.classList.remove("wondering");
 
-	}, 5000);
-})();
+// 	}, 5000);
+// })();
 
 //ускорение hover
 
@@ -32,4 +32,18 @@ function hover(){
 function noHover(){
 	mainFace.classList.remove("looking");
 };
+
+function blinking(){
+
+	setInterval(() => {
+		setTimeout(() => {
+			mainFace.style.backgroundImage = "url(../img/emotions/blinking.png)"
+		}, 2000);
+		setTimeout(() => {
+			mainFace.style.backgroundImage = ""
+		}, 2260);
+	}, 3500);
+}
+
+blinking()
 
